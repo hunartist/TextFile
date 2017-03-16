2bab24cb7e0d17dd43007c078e3f298c732e5579
hunartist <hunartist@gmail.com>
2017-03-16 21:14:17 +0800

    添加项目文件。

---
+++ TestQuery.sql
@@ -0,0 +1,6 @@
+﻿select * from RoomApply
+select * from RoomApplySub
+
+--select distinct a.strRoom,a.intDay,a.intPeriodNum,a.strName,a.strClass,a.strTeacher,a.intStartWeek,a.intEndWeek,a.strApplication from RoomApply a inner join RoomApplySub s on a.Id=s.F_id
+
+select a.strRoom,a.intDay,a.intStartNum,a.intEndNum,a.strName,a.strClass,a.strTeacher from RoomApply a inner join RoomApplySub s on a.Id=s.F_id 
