da99179b915a7e9f91ada5e8a39faa6f793e9717
hunartist <hunartist@gmail.com>
2017-03-28 08:18:37 +0800

    修复：其他周有记录，但当前查询周无记录的教室不显示

--- roomapply.aspx.cs
+++ roomapply.aspx.cs
@@ -54,7 +54,7 @@ public partial class roomapply : System.Web.UI.Page
     {
         SqlConnection con = CommonClass.GetSqlConnection();
         SqlDataAdapter sda = new SqlDataAdapter();
-        sda.SelectCommand = new SqlCommand("select * from (select distinct s.intWeek, a.strRoom,a.intDay,a.intStartNum,a.intEndNum,a.strName,a.strClass,a.strTeacher from RoomApply a inner join RoomApplySub s on a.id = s.F_id) as aa right join RoomDetail d on aa.strRoom = d.strRoomName where (aa.intWeek = "+weekNum+ " or aa.intWeek is null) and d.strRoomName = '" + RoomName+ "' and d.strDepart= '" + departmentName+"'", con);
+        sda.SelectCommand = new SqlCommand("select * from (select distinct s.intWeek, a.strRoom,a.intDay,a.intStartNum,a.intEndNum,a.strName,a.strClass,a.strTeacher from RoomApply a inner join RoomApplySub s on a.id = s.F_id) as aa right join RoomDetail d on aa.strRoom = d.strRoomName and aa.intWeek = " + weekNum + " where d.strRoomName = '" + RoomName+ "' and d.strDepart= '" + departmentName+"'", con);
         DataSet ds = new DataSet();
         sda.Fill(ds);
         DataTable table = new DataTable();
