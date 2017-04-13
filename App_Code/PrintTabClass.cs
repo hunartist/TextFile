9c0643d784448573e084a098ea503b33168725aa
hunartist <hunartist@gmail.com>
2017-04-13 09:57:56 +0800

    printtab公共类

---
+++ App_Code/PrintTabClass.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// PrintTab 的摘要说明
+/// </summary>
+public class PrintTabClass
+{
+    public static DataTable PrintTab_SUM_DT(int weekNum, string departmentName,string sqlstr)
+    {
+        SqlConnection con = CommonClass.GetSqlConnection();
+        SqlDataAdapter sda = new SqlDataAdapter();
+        sda.SelectCommand = new SqlCommand(sqlstr, con);
+        DataSet ds = new DataSet();
+        sda.Fill(ds);
+        DataTable table = new DataTable();
+        table = ds.Tables[0];
+        DataTable dtSchedule = new DataTable();
+
+
+        //添加八列
+        dtSchedule.Columns.Add("查询");
+        for (int i = 1; i < 8; i++)
+        {
+            dtSchedule.Columns.Add(Convert.ToString(i));
+        }
+
+        //添加10行
+        for (int i = 0; i < 10; i++)
+        {
+            dtSchedule.Rows.Add();
+        }
+
+        //添加左侧固定信息（第几节）
+        for (int i = 0; i < 10; i++)
+        {
+            dtSchedule.Rows[i][0] = (i + 1);
+        }
+
+        //此数组用于存放需要合并的单元格信息。如：需要合并第一列的一、二单元格
+        //那么，数组中一行的三个数分别为1,1,2
+        int[][] tempArray = new int[table.Rows.Count][];
+        for (int i = 0; i < table.Rows.Count; i++)
+        {
+            tempArray[i] = new int[3];
+            for (int j = 0; j < 3; j++)
+            {
+                tempArray[i][j] = 0;
+            }
+        }
+
+        //遍历table，将每条课表信息填在tab中适当的位置。
+        for (int i = 0; i < table.Rows.Count; i++)
+        {
+            //Day
+            string week = Convert.ToString(table.Rows[i]["intDay"]);
+            //StartNum
+            string startNum = Convert.ToString(table.Rows[i]["intStartNum"]);
+            //EndNum
+            string endNum = Convert.ToString(table.Rows[i]["intEndNum"]);
+
+            for (int weekCount = 1; weekCount < 8; weekCount++)//确定本条数据将来显示在哪一列
+            {
+                if (week == Convert.ToString(dtSchedule.Columns[weekCount].ColumnName))//跟星期做比较，确定数据应该写在那一列
+                {
+                    tempArray[i][0] = weekCount;//记录星期（确定将来的数据显示在哪一列）
+                    break;
+                }
+            }
+
+            for (int j = 0; j < dtSchedule.Rows.Count; j++)//确定课程的开始时间和结束时间，并填写数据
+            {
+                string section = Convert.ToString(dtSchedule.Rows[j][0]);//当前行是第几节课
+                if (section == startNum)//判断课程开始时间，确定位置，填写数据
+                {
+                    tempArray[i][1] = j;//记录上课开始时间（确定数据显示在哪一行）
+                    dtSchedule.Rows[j][tempArray[i][0]] = dtSchedule.Rows[j][tempArray[i][0]].ToString() + Convert.ToString(table.Rows[i]["strRoom"]) + "<br />";
+                }
+                if (section == endNum)//判断课程结束时间，记录位置
+                {
+                    tempArray[i][2] = j;//记录课结束时间
+                    dtSchedule.Rows[j][tempArray[i][0]] = dtSchedule.Rows[j][tempArray[i][0]].ToString() + Convert.ToString(table.Rows[i]["strRoom"]) + "<br />";
+                    break;
+                }
+            }
+        }
+
+        //修改行标题
+        for (int i = 1; i < 8; i++)
+        {
+            dtSchedule.Columns[i].ColumnName = "星期" + CommonClass.WeekConvertToChinese(i);
+        }
+        //修改列标题
+        for (int i = 0; i < 10; i++)
+        {
+            dtSchedule.Rows[i][0] = "第" + CommonClass.ConvertToChinese(i + 1) + "节";
+        }
+
+        //dispose        
+        table.Dispose();
+        ds.Dispose();
+        sda.Dispose();
+        con.Dispose();
+
+        return dtSchedule;
+
+    }
+}
